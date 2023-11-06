ab844e231e93d75a4edb2a559c6ffa43a5bd4006
Warren Wisbeach <warren.wisbeach@gmail.com>
2023-11-06 13:53:51 +0000

    small updates to navbar

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
         sessions: 'users/sessions'
   }
-
+  
   # Static route for the About page
   get 'about', to: 'pages#about'
 
   get 'shop', to: 'pages#shop'
+  get 'contact', to: 'pages#contact', as: 'contact'
 
   # Root route
   root 'posts#index'
